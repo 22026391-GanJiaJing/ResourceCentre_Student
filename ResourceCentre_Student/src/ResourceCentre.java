30b08613d1d8a3bc8f1612c52d8b128c9cc5e8b3
22026391 <22026391@LAPTOP-AKO8QBM6>
2023-07-14 15:10:28 +0800

    de

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -157,8 +157,8 @@ public class ResourceCentre {
 				output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
 						chromebookList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
-			}
+					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs()); 
+			 }
 		}
 		return output;
 	}
